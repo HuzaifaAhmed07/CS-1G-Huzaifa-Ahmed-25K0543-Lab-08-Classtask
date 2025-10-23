fb63454c423f58f87ba46c55f140335821b09219
Huzaifa Ahmed <k250543@nu.edu.pk>
2025-10-23 12:04:39 +0500

    Add files via upload

---
+++ q1.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+   int i,j,num;
+   printf("Multiplcation table enter number:");
+   scanf("%d",&num);
+   for(i=1;i<=num;i++){
+    printf("%d:",i);
+    for(j=1;j<=5;j++){
+        printf("%d\t",i*j);
+    }
+    printf("\n");
+   }
+return 0;
+}
